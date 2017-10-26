84359fd6c61ba0740a0a02392ccea90c8747e524
Gu-youngfeng <yongfeng_gu@163.com>
2017-10-26 14:52:14 +0800

    Update Single.java
    Add new evaluation method to single.java, and get average results of 1 project by its corresponding 10 datasets.

--- src/sklse/yongfeng/experiments/Single.java
+++ src/sklse/yongfeng/experiments/Single.java
@@ -3,6 +3,7 @@ package sklse.yongfeng.experiments;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Random;
+import sklse.yongfeng.experiments.Overall;
 
 import sklse.yongfeng.data.FilesSearcher;
 import weka.classifiers.Classifier;
@@ -20,28 +21,34 @@ import weka.filters.supervised.instance.SMOTE;
 
 /***
  * <p>Class <b>Single</b> is used to get 10-fold cross validation result of 500 crashes from each project.</p>
- * <p>We use <b>SMOTE</b> strategy combined with other classifiers( including <b>Random forest, C4.5, 
- * Bayesnet, SMO, Kstar, SVM</b> ) to train the model. </p>
- * <p>To better understand the evaluation process, we merge all function into {@link#main(String[])} function.</p>
- *
+ * <p>We use <b>SMOTE</b> strategy combined with other 6 classifiers( including <b>Random forest, J48, 
+ * Bayes Net, SMO, KStar, SVM</b> ) to train the model. 
+ * In {@link#main(String[])} class we provide 2 kinds of method to conduct 10-fold cross-validation in each single project. </p>
+ * <h3><b>(i)</b> Directly get evaluation results from 1 dataset(each project has 1 dataset);</h3> 
+ * <h3><b>(ii)</b> Calculate the average evaluation results from 10 datasets(each project has 10 datasets).</h3>
+ * @version To be uploaded
  */
 public class Single {
 	
+	/** To save all 10 results in one project**/
 	private static double[][] results = new double[60][7];
 	
+	/** To save all 6 classifiers' name*/
 	private static String[] classifiers = {"C4.5", "RandForest", "BayesNet", "SMO", "KStar", "SVM"};
 
 	public static void main(String[] args) throws Exception {
 		
-		/** We can simply 1 single arff to results in getEvalResult*/
+		/** (i) Directly get evaluation results from 1 dataset(each project has 1 dataset) */
+		//TODO: You must change the absolute path in search method.
 //		String[] paths = {"files/codec500.arff",
 //				"files/ormlite500.arff", "files/jsqlparser500.arff", "files/collections500.arff",
 //				"files/io500.arff", "files/jsoup500.arff", "files/mango500.arff"};
 //		for(String path: paths){
-//			getEvalResult(path);
+//			getEvalResult(path, 0);
 //		}
 		
-		/** Or we can use 10 generated arff to get average results in getEvalResult*/
+		/** (ii) Calculate the average evaluation results from 10 datasets(each project has 10 datasets)*/
+		//TODO: You must change the absolute path in search method.
 		String[] pathsCode = FilesSearcher.search("D:/Users/LEE/Desktop/New_Data/", "codec");
 		String[] pathsORM = FilesSearcher.search("D:/Users/LEE/Desktop/New_Data/", "ormlite");
 		String[] pathsJSQ = FilesSearcher.search("D:/Users/LEE/Desktop/New_Data/", "jsqlparser");
@@ -50,24 +57,23 @@ public class Single {
 		String[] pathsJSO = FilesSearcher.search("D:/Users/LEE/Desktop/New_Data/", "jsoup");
 		String[] pathsMAN = FilesSearcher.search("D:/Users/LEE/Desktop/New_Data/", "mango");
 		
-		List<String[]> dataCollection = new ArrayList<>();
+		List<String[]> dataCollection = new ArrayList<>(); // add all 7 path arrays into the list
 		dataCollection.add(pathsCode);
 		dataCollection.add(pathsORM);
 		dataCollection.add(pathsJSQ);
 		dataCollection.add(pathsCOL);
 		dataCollection.add(pathsIO);
 		dataCollection.add(pathsJSO);
-		dataCollection.add(pathsMAN);
+		dataCollection.add(pathsMAN);	
 		
-		
-		for(String[] dataset: dataCollection){
+		for(String[] dataset: dataCollection){ // for each project
 			int index = 0;
-			for(String arffs: dataset){
+			for(String arffs: dataset){ // each project has 10 arff files
 				getEvalResult(arffs, index);
 				index += 6;
 			}
 			
-			for(int j=0; j<6; j++){
+			for(int j=0; j<6; j++){ // for each classifier
 				double p0 = 0.0d, 
 					   p1 = 0.0d, 
 					   r0 = 0.0d, 
@@ -75,7 +81,7 @@ public class Single {
 					   f0 = 0.0d,
 					   f1 = 0.0d,
 					   acc = 0.0d;
-				for(int i=j; i<60; i+=6){	
+				for(int i=j; i<60; i+=6){	// for each time
 					p0 += results[i][0];
 					r0 += results[i][1];
 					f0 += results[i][2];
@@ -84,9 +90,10 @@ public class Single {
 					f1 += results[i][5];
 					acc += results[i][6];
 				}
+				
 				System.out.printf("%-15s: %6.3f, %6.3f, %6.3f, %6.3f, %6.3f, %6.3f, %6.3f\n", 
 						classifiers[j], p0*1.0/10.0, r0*1.0/10.0, f0*1.0/10.0, p1*1.0/10.0, r1*1.0/10.0, f1*1.0/10.0, acc*1.0/10.0);
-				
+				//print the average of 10 times
 			}
 		}
 				
@@ -94,7 +101,7 @@ public class Single {
 	
 	/***
 	 * <p>To get 10-fold cross validation in one single arff in <b>path</b></p>
-	 * @param path arff file
+	 * @param path dataset path
 	 * @throws Exception
 	 */
 	public static void getEvalResult(String path, int index) throws Exception{
@@ -133,7 +140,7 @@ public class Single {
 			
 			eval.crossValidateModel(fc, ins, 10, new Random(1));
 			
-//			System.out.printf("%-15s: ", clfName);
+//			System.out.printf("%-15s: ", classifiers[i]);
 //			System.out.printf(" %4.3f %4.3f %4.3f", eval.precision(0), eval.recall(0), eval.fMeasure(0));
 //			System.out.printf(" %4.3f %4.3f %4.3f", eval.precision(1), eval.recall(1), eval.fMeasure(1));
 //			System.out.printf(" %4.3f \n\n", (1-eval.errorRate()));
