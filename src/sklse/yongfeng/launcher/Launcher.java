2bdc8bc93e2eb89aee4fff70fad82638698c75a2
BUGFIGHTER_YF <yongfeng_gu@163.com>
2017-12-04 16:15:05 +0800

    Command Correction
    change the java -jar command due to the rename of the project.

--- src/sklse/yongfeng/launcher/Launcher.java
+++ src/sklse/yongfeng/launcher/Launcher.java
@@ -40,27 +40,28 @@ public class Launcher {
 					TopTenFeatureEvaluation.main(null);
 					break;
 				default:
-					System.out.println("[ERROR!] >> WrongCommandError! Try to use command: [java -jar WekaEvaluation.jar -help] ");
+					System.out.println("[ERROR!] >> WrongCommandError! Try to use command: [java -jar CraTer.jar -help] ");
 					break;
 				}
 				
 			}else{
-				System.out.println("[ERROR!] >> CommandNotFoundError! Try to use command: [java -jar WekaEvaluation.jar -help] ");
+				System.out.println("[ERROR!] >> CommandNotFoundError! Try to use command: [java -jar CraTer.jar -help] ");
 			}
 		}catch(Exception e){
-			System.out.println("[ERROR!] >> DatasetNotFoundError! Please make sure that the -jar is in the root directory of WekaEvaluation project.");
+			System.out.println("[ERROR!] >> DatasetNotFoundError! Please make sure that the -jar is in the root directory of CraTer project.");
 		}
 	}
 	
 	public static void messageHELP(){
 		System.out.println("\n-------------------------------------------------\n");
 		System.out.println(">> PROJECT NAME: CraTer\n");
-		System.out.println(">> DESCRIPTION : This project implements the EVALUATION PROCESS in paper [Does the Fault Reside in Stack Trace]\n");
+		System.out.println(">> DESCRIPTION : This project implements the EVALUATION PROCESS in paper [Does the fault reside in stack trace]. ");
+		System.out.println("   The latest versions is updated on github [https://github.com/Gu-Youngfeng/CraTer].\n");
 		System.out.println(">> USAGE       : -help       Output the help message of CraTer.");
-		System.out.println("               : -total      Evaluate the total datasets by using [classifiers] + SMOTE.");
-		System.out.println("               : -single     Evaluate the single project by using [classifiers] + SMOTE.");
-		System.out.println("               : -imbalance  Evaluate the impact of imbalanced data processing methods on the results.");
-		System.out.println("               : -feature    Evaluate the impact of feature selction methods on the results.");
+		System.out.println("               : -total      Evaluate the total datasets combined with 7 projects.");
+		System.out.println("               : -single     Evaluate the datasets from each project.");
+		System.out.println("               : -imbalance  Evaluate the impact of imbalanced data processing methods on the prediction results.");
+		System.out.println("               : -feature    Evaluate the impact of feature selction methods on the prediction results.");
 		System.out.println("               : -rank       Rank the top-10 feature list of each project.");
 		System.out.println("               : -tenEval    Evaluate each project only using top-10 features");
 		System.out.println("\n-------------------------------------------------\n");
