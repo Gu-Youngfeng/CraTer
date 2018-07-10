f422bedd1c7af830efbe41890de9311cada25d76
yongfeng <yongfeng_gu@163.com>
2018-07-10 20:19:46 +0800

    update Launcher.java

--- src/sklse/yongfeng/launcher/Launcher.java
+++ src/sklse/yongfeng/launcher/Launcher.java
@@ -20,7 +20,7 @@ public class Launcher {
 			if(args != null && args.length == 1){
 				switch(args[0]){
 				case "-help":
-					messageHELP();
+					printHELP();
 					break;
 				case "-analysis":
 					Analysis.main(null);
@@ -44,32 +44,56 @@ public class Launcher {
 					TopTenFeatureEvaluation.main(null);
 					break;
 				default:
-					System.out.println("[ERROR!] >> WrongCommandError! Try to use command: [java -jar CraTer.jar -help] ");
+					printHELP();
 					break;
 				}
 				
 			}else{
-				System.out.println("[ERROR!] >> CommandNotFoundError! Try to use command: [java -jar CraTer.jar -help] ");
+				printLOGO();
 			}
 		}catch(Exception e){
 			System.out.println("[ERROR!] >> DatasetNotFoundError! Please make sure that the -jar is in the root directory of CraTer project.");
 		}
 	}
 	
-	public static void messageHELP(){
-		System.out.println("\n-------------------------------------------------\n");
-		System.out.println(">> PROJECT NAME: CraTer\n");
-		System.out.println(">> DESCRIPTION : This project implements the EVALUATION PROCESS in paper [Does the fault reside in stack trace]. ");
-		System.out.println("   The latest versions is updated on github [https://github.com/Gu-Youngfeng/CraTer].\n");
-		System.out.println(">> USAGE       : -help       Output the help message of CraTer.");
-		System.out.println("               : -analysis   Conduct static analysis of 7 projects.");
-		System.out.println("               : -total      Evaluate the total datasets combined with 7 projects.");
-		System.out.println("               : -single     Evaluate the datasets from each project.");
-		System.out.println("               : -imbalance  Evaluate the impact of imbalanced data processing methods on the prediction results.");
-		System.out.println("               : -feature    Evaluate the impact of feature selction methods on the prediction results.");
-		System.out.println("               : -rank       Rank the top-10 feature list of each project.");
-		System.out.println("               : -tenEval    Evaluate each project only using top-10 features");
-		System.out.println("\n-------------------------------------------------\n");
+	public static void printHELP(){
+		System.out.println("----------------------------------------------------------------------------------------------------------------");
+		System.out.println("    PARAMETERS | [default]   Print the description of CraTer");
+		System.out.println("               | -help       Print the parameter list of CraTer.");
+		System.out.println("               | -analysis   Conduct static analysis of 7 projects.");
+		System.out.println("               | -total      Evaluate the total datasets combined with 7 projects.");
+		System.out.println("               | -single     Evaluate the datasets from each project.");
+		System.out.println("               | -imbalance  Evaluate the impact of imbalanced data processing methods on the prediction results.");
+		System.out.println("               | -feature    Evaluate the impact of feature selction methods on the prediction results.");
+		System.out.println("               | -rank       Rank the top-10 feature list of each project.");
+		System.out.println("               | -tenEval    Evaluate each project only using top-10 features");
+		System.out.println("----------------------------------------------------------------------------------------------------------------");
+	}
+	
+	public static void printLOGO(){
+		System.out.println("_________________________________________________________________________________________________________________");
+		System.out.println("   00000    000000         00     000000000   000000    000000   ");
+		System.out.println("  000       00   00       0000        00      00        00   00  ");
+		System.out.println("  00        000000       00  00       00      000000    000000   ");
+		System.out.println("  000       00   00     00000000      00      00        00   00  ");
+		System.out.println("   00000    00    00   00      00     00      000000    00    00 ");
+		System.out.println("_________________________________________________________________________________________________________________");
+		System.out.println("      PROJECT | CraTer");
+		System.out.println("______________|__________________________________________________________________________________________________");
+		System.out.println("  DESCRIPTION | This project is the proptotype proposed in paper [Does the fault reside in a stack trace]. ");
+		System.out.println("              | The latest versions is updated on github   [https://github.com/Gu-Youngfeng/CraTer].");
+		System.out.println("______________|__________________________________________________________________________________________________");
+		System.out.println("   PARAMETERS | [default]   Print the description of CraTer");
+		System.out.println("              | -help       Print the parameter list of CraTer.");
+		System.out.println("              | -analysis   Conduct static analysis of 7 projects.");
+		System.out.println("              | -total      Evaluate the total datasets combined with 7 projects.");
+		System.out.println("              | -single     Evaluate the datasets from each project.");
+		System.out.println("              | -imbalance  Evaluate the impact of imbalanced data processing methods on the prediction results.");
+		System.out.println("              | -feature    Evaluate the impact of feature selction methods on the prediction results.");
+		System.out.println("              | -rank       Rank the top-10 feature list of each project.");
+		System.out.println("              | -tenEval    Evaluate each project only using top-10 features");
+		System.out.println("______________|__________________________________________________________________________________________________");
+		System.out.println("");
 	}
 
 }
